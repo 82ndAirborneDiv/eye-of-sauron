4ba0e52ac8c6e02b6a8f20d8f3ad37e29a81c8fa
Michael Ta <technical.ta@gmail.com>
2016-11-08 16:20:08 -0500

    update dash component

--- src/app/features/dashboard.component.ts
+++ src/app/features/dashboard.component.ts
@@ -5,46 +5,79 @@ import { Observable } from 'rxjs/Observable';
 
 import { AppState } from '../reducers';
 import { Store } from '@ngrx/store';
-import { UserActions } from '../user/user.actions';
-import { User } from '../user/user.model';
+// import { UserActions } from '../user/user.actions';
+// import { User } from '../user/user.model';
+import { Site } from '../sites/site.model';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [`#my-logout-button { background: #F44336 }`]
+  styleUrls: ['./dashboard.component.css']
 })
 
 export class DashboardComponent implements OnDestroy, OnInit {
   destroyed$: Subject<any> = new Subject<any>();
   form: FormGroup;
   nameLabel = 'Enter your name';
-  user: User;
-  user$: Observable<User>;
+  // user: User;
+  // user$: Observable<User>;
   constructor(
     fb: FormBuilder,
     private store: Store<AppState>,
-    private userActions: UserActions,
+    // private userActions: UserActions,
   ) {
     this.form = fb.group({
       name: ''
     });
-    this.user$ = this.store.select(state => state.user.user);
-    this.user$.takeUntil(this.destroyed$)
-      .subscribe(user => { this.user = user; });
+    // this.user$ = this.store.select(state => state.user.user);
+    // this.user$.takeUntil(this.destroyed$)
+    // .subscribe(user => { this.user = user; });
   }
 
+  monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  currentDate = '';
+  currentTime = '';
+
   ngOnInit() {
-    this.form.get('name').setValue(this.user.name);
+    // this.form.get('name').setValue(this.user.name);
+    this.getCurrentTime();
+    this.getCurrentDate();
   }
 
   logout() {
-    this.store.dispatch(this.userActions.logout());
+    // this.store.dispatch(this.userActions.logout());
+  }
+
+  getCurrentDate() {
+    setInterval(() => {
+      var d = new Date();
+
+      this.currentDate = this.monthNames[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+    },
+      1000)
+  }
+
+  getCurrentTime() {
+    setInterval(() => {
+      var time = new Date();
+      var hours = time.getHours();
+      var minutes = time.getMinutes();
+      var s = time.getSeconds();
+
+      var ampm = hours <= 11 ? ' AM' : ' PM';
+      var strTime = [(hours === 12 ? "12" : hours % 12),
+      (minutes < 10 ? "0" + minutes : minutes)
+      ].join(':') + ampm;
+
+      this.currentTime = strTime;
+    },
+      1000)
   }
 
   submitState() {
-    this.store.dispatch(this.userActions.editUser(
-      Object.assign({}, this.user, { name: this.form.get('name').value }
-      )));
+    // this.store.dispatch(this.userActions.editUser(
+    //   Object.assign({}, this.user, { name: this.form.get('name').value }
+    //   )));
   }
 
   ngOnDestroy() {
