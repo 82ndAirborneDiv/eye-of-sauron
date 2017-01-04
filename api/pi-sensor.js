390f19ed1985208342dfa90a95934589e553238b
Michael Ta <technical.ta@gmail.com>
2017-01-04 08:57:59 -0500

    added thresholdCount condition to

--- api/pi-sensor.js
+++ api/pi-sensor.js
@@ -6,6 +6,7 @@ var moment = require('moment');
 var mailer = require('../labmonitor/lib/mailer')();
 
 var notificationTime;
+var thresholdCount = 0;
 
 module.exports.getRoutes = function () {
   var router = express.Router();
@@ -20,9 +21,13 @@ module.exports.getRoutes = function () {
 
         if (currentTemp > 93) {
           // if (true) {  //for testing
-          notificationTime = +new Date;
-
-          mailer.sendTempWarning(currentTemp, notificationTime);   //currently disabled until I see how they want to deal with notifcations
+          thresholdCount++;
+          
+          if(thresholdCount > 5) {
+            notificationTime = +new Date;
+            mailer.sendTempWarning(currentTemp, notificationTime);   //currently disabled until I see how they want to deal with notifcations
+            thresholdCount = 0;
+          }
         }
 
 
